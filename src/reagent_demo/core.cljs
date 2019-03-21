ef8e85873f6f4378e041ba3b8103792e45c9944a
Coby Tamayo <ctamayo@sitecrafting.com>
2019-03-21 09:24:08 -0700

    Add an "Add!" button

--- src/reagent_demo/core.cljs
+++ src/reagent_demo/core.cljs
@@ -9,13 +9,28 @@
                                      {:id "YT-1300" :name "Millennium Falcon"}
                                      {:id "OV-204" :name "Atlantis"}]
 
+                        :new-ship {:id "ABC" :name "My Very Own Spaceship"}
+
                         :selection {}}))
 
 (defn handler [current-state action value]
   "Transforms the current app-state into the desired new state according to
   action and value"
   (case action
-    :update-ship (assoc current-state :selection value)))
+    :update-ship (assoc current-state :selection value)
+
+    :update-new-ship-name (assoc-in current-state [:new-ship :name] value)
+
+    :update-new-ship-id (assoc-in current-state [:new-ship :id] value)
+
+    ; this one's a little more advanced.
+    ; We want to update two pieces of state: The new-ship and our list of
+    ; spaceships. The list of spaceships should get the new value added to it,
+    ; while new-ship should get reset to a blank value.
+    :add-ship (-> current-state
+                  ; reset new-ship
+                  (assoc :new-ship {})
+                  (update :spaceships (fn [ships] (vec (conj ships value)))))))
 
 (defn emit! [action value]
   "Get the new, transformed app state according to action and value,
@@ -38,6 +53,23 @@
       [:strong (str "You said it! The best spaceship is: " name " (ID: " id ")")])
     [:p "Who knows what the best spaceship is??"]))
 
+(defn ship-adder [new-ship]
+  [:div {:class "new-ship"}
+   [:h4 "Add a ship:"]
+   [:label {:for "new-ship-id"} "ID:"]
+   [:br]
+   [:input {:on-change #(emit! :update-new-ship-id (.-target.value %))
+            :value (:id new-ship)
+            :id "new-ship-id"}]
+   [:br]
+   [:label {:for "new-ship-name"} "Name:"]
+   [:br]
+   [:input {:on-change #(emit! :update-new-ship-name (.-target.value %))
+            :value (:name new-ship)
+            :name "new-ship-name"}]
+   [:br]
+   [:button {:on-click #(emit! :add-ship new-ship)} "Add!"]])
+
 (defn home-page []
   [:div
    [:h2 "Welcome to Reagent on Lando!"]
@@ -56,7 +88,8 @@
         [spaceship ship])
       ; The collection of things we're mapping over:
       (:spaceships @state))]
-   [best-spaceship (:selection @state)]])
+   [best-spaceship (:selection @state)]
+   [ship-adder (:new-ship @state)]])
 
 ;; -------------------------
 ;; Initialize app
